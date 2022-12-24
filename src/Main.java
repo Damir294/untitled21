de9e9faf23dc31c2cb0631efd906e31e2a208951
Дамир <damir.obuhov@mail.ru>
2022-12-25 00:08:40 +0400

    LC2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // Демонстрация обработки исключений,
+        class ExcDemol {
+            public static void main (String args[]) throws ArraylndexOutOfBoundsException {
+                int nums[] = new int[4];
+
+                // Создание блока try.
+                System.out.println("Before exception is generated.");
+
+                // Попытка обратиться за границы массива.
+                nums[7] = 10;
+                System.out.println("this won't be displayed");
+                System.out.println("After catch statement.");
+            }
+        }
+
     }
 }
